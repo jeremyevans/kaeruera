ce0f7ff5767979fae53592aee92f1d8ae65c880f
Jeremy Evans <code@jeremyevans.net>
2016-12-31 20:46:13 -0800

    Freeze datasets and disable identifier mangling

--- db.rb
+++ db.rb
@@ -4,5 +4,6 @@ module KaeruEra; end
 begin
   load File.join(File.dirname(__FILE__), 'db_config.rb')
 rescue LoadError
-  KaeruEra::DB = Sequel.connect(ENV['KAERUERA_DATABASE_URL'] || ENV['DATABASE_URL'] || "postgres:///#{'kaeruera_test' if ENV['RACK_ENV'] == 'test'}?user=kaeruera")
+  KaeruEra::DB = Sequel.connect(ENV['KAERUERA_DATABASE_URL'] || ENV['DATABASE_URL'] || "postgres:///#{'kaeruera_test' if ENV['RACK_ENV'] == 'test'}?user=kaeruera", :identifier_mangling=>false)
 end
+KaeruEra::DB.extension(:freeze_datasets)
