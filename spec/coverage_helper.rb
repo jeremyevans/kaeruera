fe77b0e78e43bce12a9f1442095af6a5e5e6f76a
Jeremy Evans <code@jeremyevans.net>
2025-08-22 14:38:24 -0700

    Use SimpleCov.add_filter block instead of string
    Using a string is almost always a bad idea, because if any part of
    the path contains the string, it will filter out the path. Switch
    to using a block and doing a match on the entire path prefix.
    
    Additionally, instead of just filtering out spec files, filter out
    all files except those in the lib directory, since this is only
    used to check coverage for the files in the kaeruera gem.

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -5,7 +5,7 @@ if command = ENV.delete('COVERAGE')
   SimpleCov.start do
     enable_coverage :branch
     command_name command
-    add_filter "/spec/"
+    add_filter{|f| !f.filename.match(%r{\A#{Regexp.escape(File.dirname(__dir__))}/lib/})}
     add_group('Missing'){|src| src.covered_percent < 100}
     add_group('Covered'){|src| src.covered_percent == 100}
   end
